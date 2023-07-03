3191f627cc27bc3847d82f19871b4886e72a1bd2
shubham <shubham.bhoyar077@gmail.com>
2023-07-03 15:37:08 +0530

    Generate groups controller with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root "groups#index"
+  resources :groups, only: [:show, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
