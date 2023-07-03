672743dd48200a5e163249eebe176b4c69debea8
shubham <shubham.bhoyar077@gmail.com>
2023-07-03 14:29:15 +0530

    Add users table to author_id

--- db/migrate/20230703085802_create_products.rb
+++ db/migrate/20230703085802_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.decimal :amount
 
       t.timestamps
