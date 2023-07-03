c2da9186ed99b1dd70b189126a650baf0297f6f9
shubham <shubham.bhoyar077@gmail.com>
2023-07-03 14:54:44 +0530

    Add association in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :group, foreign_key: 'author_id'
-  has_many :product, foreign_key: 'author_id'
+  has_many :groups, foreign_key: 'author_id'
+  has_many :products, foreign_key: 'author_id'
 end
