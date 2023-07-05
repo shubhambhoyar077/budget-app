fdd6d6d8ce864d81ae7734e9ba6d09dbfb3e915a
shubham <shubham.bhoyar077@gmail.com>
2023-07-05 08:59:12 +0530

    Add test for product model validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  @user = User.create(name: 'Tom', email: 'test2@gmail.com', password: 'test123')
+  subject { Product.new(author: @user, name: "test product", amount: 20) }
+
+  before { subject.save }
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Name should contan some char' do
+    subject.name = '    '
+    expect(subject).to_not be_valid
+  end
+
+  it 'amount should be greated than zero' do
+    subject.amount = -1
+    expect(subject).to_not be_valid
+  end
 end
