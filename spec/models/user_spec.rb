97a22d92ae673211520f3f1d785ad9681368384b
shubham <shubham.bhoyar077@gmail.com>
2023-07-05 08:58:51 +0530

    Add model test for user to test validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Tom', email: 'test2@gmail.com', password: 'test123') }
+
+  before { subject.save }
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Name should contan some char' do
+    subject.name = '    '
+    expect(subject).to_not be_valid
+  end
+
+  it 'email should be present' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
 end
